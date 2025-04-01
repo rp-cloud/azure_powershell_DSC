1092c35834e0df3749e2d14a8b846ab175316a7e
Rafał <119082735+rp-cloud@users.noreply.github.com>
2025-04-01 21:54:28 +0200

    Update nginx.ps1

--- PowerShell/nginx.ps1
+++ PowerShell/nginx.ps1
@@ -1,8 +1,11 @@
 Configuration InstallNginx {
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
+
     Node "localhost" {
         Package Nginx {
             Ensure = "Present"
             Name = "nginx"
+            Path = "/usr/bin/nginx"   # <-- Poprawiona ścieżka do binarki Nginx
             PackageManager = "apt"
         }
 
